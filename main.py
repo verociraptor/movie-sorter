a07fce6504be5a712e7c55d1091b2340aae786d0
verociraptor <vparr007@fiu.edu>
2020-08-14 20:10:47 -0400

    Added an in-progress movie list view TODO: fix and allow a specific layout for a movie item in the list view

--- main.py
+++ main.py
@@ -3,14 +3,48 @@ import sys
 import os
 
 from MovieServer import movie_sorter as ms
-from PySide2.QtWidgets import QApplication, QWidget, QPushButton
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QHBoxLayout, QListWidgetItem
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
 
-list = []
 
-for i in range(0, 5):
-    list.append(ms.Movie("Jose's Delivery Service", 2018))
+
+class MovieItem(QListWidgetItem):
+    def __init__(self, title, year):
+        super(MovieItem, self).__init__()
+        self.init_widget(title, year)
+
+    def init_widget(self, title, year):
+        title = QLabel(title)
+        year = QLabel(year)
+        movieBox = QHBoxLayout()
+        movieBox.addWidget(title)
+        movieBox.addWidget(year)
+        self.setLayout(movieBox)
+
+class ItemsList(QWidget):
+    def __init__(self, items):
+        super(ItemsList, self).__init__()
+        self.init_widget(items)
+
+    def init_widget(self, items):
+        #creates box to list all items
+        listBox = QVBoxLayout(self)
+        self.setLayout(listBox)
+
+        #makes the list box scrollable and resizeable to fit space
+        scroll = QScrollArea(self)
+        listBox.addWidget(scroll)
+        scroll.setWidgetResizable(True)
+
+        scrollContent = QWidget(scroll)
+
+        scrollLayout = QVBoxLayout(scrollContent)
+        scrollContent.setLayout(scrolLLayout)
+        for item in items:
+            scrollLayout.addWidget(item)
+        scroll.setWidget(scrollContent)
+
 
 class MovieApp(QWidget):
     def __init__(self):
@@ -19,6 +53,15 @@ class MovieApp(QWidget):
         self.ui.stackedWidget.setCurrentIndex(0)
         self.ui.toSearchPage.clicked.connect(lambda:  self.ui.stackedWidget.setCurrentIndex(1))
         self.ui.mainMenuButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
+        for i in range(0, 5):
+            self.ui.listWidget.addItem(MovieItem("Jose's Delivery Service", "2018"))
+
+
+    def init_movies_view(self):
+        list = []
+        for i in range(0, 5):
+            list.append(MovieItem("Jose's Delivery Service", "2018"))
+        return ItemsList(list)
 
     def load_ui(self):
         loader = QUiLoader()
@@ -28,6 +71,8 @@ class MovieApp(QWidget):
         ui = loader.load(ui_file, self)
         ui_file.close()
         return ui
+
+
 
 if __name__ == "__main__":
     app = QApplication([])
