0a64ea4009bffff1643881626cc27ec6cb8ae9b0
GatorNylon <wutangchang20@gmail.com>
2020-08-16 23:41:14 -0400

    added error handling, status, and directory finder failed to push all changes on previous push

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ import os
 
 from MovieServer import movie_sorter as ms
 from MovieServer import movie_sorter_server as server
-from PySide2.QtWidgets import QApplication, QWidget, QPushButton
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
 
 
 class test627(QWidget):
+
+
     def __init__(self):
         super(test627, self).__init__()
         self.ui = self.load_ui()
+        self.ui.dirPath.setReadOnly(True)
+        self.ui.status.setReadOnly(True)
         self.ui.stackedWidget.setCurrentIndex(0)
         self.ui.toSearchPage.clicked.connect(lambda:  self.ui.stackedWidget.setCurrentIndex(1))
         self.ui.mainMenuButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
-        self.ui.connectCache.clicked.connect(lambda: server.Connect_to_Local_Cache())
-        self.ui.createCache.clicked.connect(lambda: server.Create_Local_Cache())
+        self.ui.connectCache.clicked.connect(self.connect_to_local_cache)
+        self.ui.createCache.clicked.connect(self.create_local_cache)
+        self.ui.browseDirectories.clicked.connect(self.browse_directories)
 
     def load_ui(self):
         loader = QUiLoader()
@@ -27,6 +32,26 @@ class test627(QWidget):
         ui = loader.load(ui_file, self)
         ui_file.close()
         return ui
+
+    def create_local_cache(self):
+        try:
+            server.Create_Local_Cache()
+            self.ui.status.setText("Sucessfully created local cache")
+        except:
+            self.ui.status.setText("Error: cache already exists!")
+
+
+    def connect_to_local_cache(self):
+        try:
+            server.Connect_to_Local_Cache()
+            self.ui.status.setText("Successfully connected to local cache")
+        except:
+            self.ui.status.setText("Create a cache first!")
+
+    def browse_directories(self):
+        dirName = QFileDialog.getExistingDirectory()
+        print(dirName)
+        self.ui.dirPath.setText(dirName)
 
 if __name__ == "__main__":
     app = QApplication([])
