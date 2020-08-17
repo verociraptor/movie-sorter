0a1d27928363f30af38fbe61949d6c81bc88935e
verociraptor <vparr007@fiu.edu>
2020-08-17 10:06:20 -0400

    Added list view of movies with static data Used the already made QListWidget in form.ui and created a custom widget to fit the movie info. This custom widget is then set to a QListWidgetItem to create a unique view. This can later be edited for better looking UI. It also needs to fit dynamic data so the properties of each aforementioned widget would need to be edited.

--- main.py
+++ main.py
@@ -7,9 +7,22 @@ from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QHBoxL
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
 
+#static data to test list view functionality
+#TODO: connect to database
+list = [["Kiki", "2018"], ["Mononoke", "2019"],
+        ["M", "2017"], ["K", "2019"],
+        ["Mark", "2016"], ["Mocie2", "2011"],
+        ["Tiff", "2015"], ["Moke", "2012"],
+        ["Balogna", "2014"], ["Movie3", "2010"],
+        ["Hanna", "2013"], ["Movie4", "2009"],
+        ["Movie", "2012"], ["Movie5", "2008"]]
 
-
-class MovieItem(QListWidgetItem):
+class MovieItem(QWidget):
+    """
+    Custom widget to create the list view of movies in
+    the search page
+    TODO: attach all other info of movie and maybe make it prettier
+    """
     def __init__(self, title, year):
         super(MovieItem, self).__init__()
         self.init_widget(title, year)
@@ -22,46 +35,54 @@ class MovieItem(QListWidgetItem):
         movieBox.addWidget(year)
         self.setLayout(movieBox)
 
-class ItemsList(QWidget):
-    def __init__(self, items):
-        super(ItemsList, self).__init__()
-        self.init_widget(items)
+#No longer really needed bc figured out how to list movies using the listwidget
+#containing custom widgets
+#class ItemsList(QWidget):
+#    def __init__(self, items):
+#        super(ItemsList, self).__init__()
+#        self.init_widget(items)
 
-    def init_widget(self, items):
-        #creates box to list all items
-        listBox = QVBoxLayout(self)
-        self.setLayout(listBox)
+#    def init_widget(self, items):
+#        #creates box to list all items
+#        listBox = QVBoxLayout(self)
+#        self.setLayout(listBox)
 
-        #makes the list box scrollable and resizeable to fit space
-        scroll = QScrollArea(self)
-        listBox.addWidget(scroll)
-        scroll.setWidgetResizable(True)
+#        #makes the list box scrollable and resizeable to fit space
+#        scroll = QScrollArea(self)
+#        listBox.addWidget(scroll)
+#        scroll.setWidgetResizable(True)
 
-        scrollContent = QWidget(scroll)
+#        scrollContent = QWidget(scroll)
 
-        scrollLayout = QVBoxLayout(scrollContent)
-        scrollContent.setLayout(scrolLLayout)
-        for item in items:
-            scrollLayout.addWidget(item)
-        scroll.setWidget(scrollContent)
+#        scrollLayout = QVBoxLayout(scrollContent)
+#        scrollContent.setLayout(scrolLLayout)
+#        for item in items:
+#            scrollLayout.addWidget(item)
+#        scroll.setWidget(scrollContent)
 
 
 class MovieApp(QWidget):
+    """
+    App widget to start the whole app.
+    Sets the buttons and movie views, and
+    gets the default ui made.
+    """
     def __init__(self):
         super(MovieApp, self).__init__()
         self.ui = self.load_ui()
         self.ui.stackedWidget.setCurrentIndex(0)
         self.ui.toSearchPage.clicked.connect(lambda:  self.ui.stackedWidget.setCurrentIndex(1))
         self.ui.mainMenuButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
-        for i in range(0, 5):
-            self.ui.listWidget.addItem(MovieItem("Jose's Delivery Service", "2018"))
+        self.init_movies_view()
 
 
     def init_movies_view(self):
-        list = []
-        for i in range(0, 5):
-            list.append(MovieItem("Jose's Delivery Service", "2018"))
-        return ItemsList(list)
+        for i in list:
+            movie = MovieItem(i[0], i[1])
+            myQListItem = QListWidgetItem(self.ui.listWidget)
+            myQListItem.setSizeHint(movie.sizeHint())
+            self.ui.listWidget.addItem(myQListItem)
+            self.ui.listWidget.setItemWidget(myQListItem, movie)
 
     def load_ui(self):
         loader = QUiLoader()
