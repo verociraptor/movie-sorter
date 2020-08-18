2a9943e034887d13703b8aa02ccde0816ede8135
GatorNylon <wutangchang20@gmail.com>
2020-08-18 17:23:52 -0400

    fully functional menu

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class test627(QWidget):
     def __init__(self):
         super(test627, self).__init__()
         self.ui = self.load_ui()
+        self.clickedConnect = False   # has user clicked connect ?
         self.ui.dirPath.setReadOnly(True)
         self.ui.status.setReadOnly(True)
         self.ui.stackedWidget.setCurrentIndex(0)
@@ -23,6 +24,7 @@ class test627(QWidget):
         self.ui.connectCache.clicked.connect(self.connect_to_local_cache)
         self.ui.createCache.clicked.connect(self.create_local_cache)
         self.ui.browseDirectories.clicked.connect(self.browse_directories)
+        self.ui.exportMovies.clicked.connect(self.export_movies)
 
     def load_ui(self):
         loader = QUiLoader()
@@ -45,13 +47,24 @@ class test627(QWidget):
         try:
             server.Connect_to_Local_Cache()
             self.ui.status.setText("Successfully connected to local cache")
+            self.clickedConnect = True
         except:
             self.ui.status.setText("Create a cache first!")
 
     def browse_directories(self):
         dirName = QFileDialog.getExistingDirectory()
-        print(dirName)
         self.ui.dirPath.setText(dirName)
+
+    def export_movies(self, clickedConnect):
+        dirName = self.ui.dirPath.text()
+        if self.clickedConnect is False :
+            self.ui.status.setText("Connect to your local cache first!")
+        elif len(dirName) == 0:
+            self.ui.status.setText("No directory selected!")
+        else :
+            server.export_to_SQLMoviesTable(dirName)
+            self.ui.status.setText("Movies successfully exported")
+
 
 if __name__ == "__main__":
     app = QApplication([])
