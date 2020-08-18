395438bcce021806728a607bf88c441e2c334d3f
verociraptor <vparr007@fiu.edu>
2020-08-18 00:17:34 -0400

    Added signal call when movie is clicked on

--- main.py
+++ main.py
@@ -1,30 +1,3 @@
-# This Python file uses the following encoding: utf-8
-
-
-#containing custom widgets
-#class ItemsList(QWidget):
-#    def __init__(self, items):
-#        super(ItemsList, self).__init__()
-#        self.init_widget(items)
-
-#    def init_widget(self, items):
-#        #creates box to list all items
-#        listBox = QVBoxLayout(self)
-#        self.setLayout(listBox)
-
-#        #makes the list box scrollable and resizeable to fit space
-#        scroll = QScrollArea(self)
-#        listBox.addWidget(scroll)
-#        scroll.setWidgetResizable(True)
-
-#        scrollContent = QWidget(scroll)
-
-#        scrollLayout = QVBoxLayout(scrollContent)
-#        scrollContent.setLayout(scrolLLayout)
-#        for item in items:
-#            scrollLayout.addWidget(item)
-#        scroll.setWidget(scrollContent)
-
 import sys
 import os
 from MovieServer import movie_sorter_server as server
@@ -53,13 +26,16 @@ class MovieItem(QWidget):
         self.init_widget(title, year)
 
     def init_widget(self, title, year):
-        title = QLabel(title)
-        year = QLabel(year)
+        self.title = title
+        self.year = year
+        movie_title = QLabel(self.title)
+        movie_year = QLabel(self.year)
         movieBox = QHBoxLayout()
-        movieBox.addWidget(title)
-        movieBox.addWidget(year)
+        movieBox.addWidget(movie_title)
+        movieBox.addWidget(movie_year)
         self.setLayout(movieBox)
-        
+
+
 class MovieApp(QWidget):
     """
     App widget to start the whole app.
@@ -74,13 +50,15 @@ class MovieApp(QWidget):
         self.ui.dirPath.setReadOnly(True)
         self.ui.status.setReadOnly(True)
         self.ui.stackedWidget.setCurrentIndex(0)
+        self.connect_buttons()
+        self.init_movies_view()
+
+    def connect_buttons(self):
         self.ui.toSearchPage.clicked.connect(lambda:  self.ui.stackedWidget.setCurrentIndex(1))
         self.ui.mainMenuButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
         self.ui.connectCache.clicked.connect(self.connect_to_local_cache)
         self.ui.createCache.clicked.connect(self.create_local_cache)
         self.ui.browseDirectories.clicked.connect(self.browse_directories)
-        self.init_movies_view()
-
 
     def init_movies_view(self):
         for i in list:
@@ -89,7 +67,7 @@ class MovieApp(QWidget):
             myQListItem.setSizeHint(movie.sizeHint())
             self.ui.listWidget.addItem(myQListItem)
             self.ui.listWidget.setItemWidget(myQListItem, movie)
-        
+        self.ui.listWidget.itemClicked.connect(self.movie_clicked)
 
     def load_ui(self):
         loader = QUiLoader()
@@ -117,8 +95,11 @@ class MovieApp(QWidget):
 
     def browse_directories(self):
         dirName = QFileDialog.getExistingDirectory()
-        print(dirName)
         self.ui.dirPath.setText(dirName)
+
+    def movie_clicked(self, item):
+        movie = self.ui.listWidget.itemWidget(item)
+        print(movie.title + " is clicked!!!")
 
 if __name__ == "__main__":
     app = QApplication([])
